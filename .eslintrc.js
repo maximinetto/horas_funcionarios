ff813deef4812995613b9b84a505b5efd49f5cbe
Maximiliano Minetto <maximinetto@gmail.com>
2022-04-09 17:57:07 -0300

    typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     jest: true,
   },
   extends: ["plugin:prettier/recommended", "google", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["@typescript-eslint"],
   rules: {
     "require-jsdoc": "off",
     "prettier/prettier": "error",
