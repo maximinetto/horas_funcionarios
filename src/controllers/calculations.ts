765da79b1c867791d8ec9c2361ac433c785277d0
Maximiliano Minetto <maximinetto@gmail.com>
2022-07-24 15:57:12 -0300

    the input parameter to calculate the hours of the officials is being fixed

--- src/controllers/calculations.ts
+++ src/controllers/calculations.ts
@@ -3,12 +3,14 @@ import { calculator } from "dependencies/container";
 import { Request, Response } from "express";
 import response from "utils/response";
 
-export const createHours = asyncHandler(async (req: Request, res: Response) => {
-  const result = await calculator.execute(res.locals.value);
+export const createHours = asyncHandler(
+  async (_req: Request, res: Response) => {
+    const result = await calculator.execute(res.locals.value);
 
-  response(res, {
-    status: 201,
-    data: result,
-    message: "Calculations done successfully",
-  });
-});
+    response(res, {
+      status: 201,
+      data: result,
+      message: "Calculations done successfully",
+    });
+  }
+);
